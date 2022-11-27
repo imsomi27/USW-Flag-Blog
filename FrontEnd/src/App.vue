aebaa126dce6de6faefce12635d173b5e78ac4a1
IMSOMI <82112204+imsomi27@users.noreply.github.com>
2022-11-27 17:08:01 +0900

    Revert "SearchPage"
    This reverts commit e556392ce74b372d1a528831f09eefd6df4fb9fa.

---
+++ FrontEnd/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Main</RouterLink>
+    <RouterLink to="/list">List</RouterLink>
+    <RouterLink to="/search">Search</RouterLink>
+   
+  </nav>
+  <RouterView />
+</template>
+
+<style scoped>
+nav {
+  position: absolute;
+  color: white;
+  background-color: white;
+}
+</style>
